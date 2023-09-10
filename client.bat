57553dccadeb96eff5d53eb66f0a519a43a00b55
sree-r-one <sreeraj_ec@hotmail.com>
2023-09-11 01:14:30 +0800

    added the batch script

---
+++ client.bat
@@ -0,0 +1,5 @@
+@echo off 
+
+@REM run client
+cd .\client
+npm run dev 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="bg-sky-200 px-4 text-white hover:bg-green-300">App</div>
+    // <div className="bg-sky-200 px-4 text-white hover:bg-green-300">App</div>
+    <div className="bg-sky-100 text-white">Hello</div>
   );
 };
 
