e91aff28a7d52ba460b40b55d711d39777c8fbe0
sree-r-one <sreeraj_ec@hotmail.com>
2023-09-11 01:32:04 +0800

    updating the start batch file

--- start.bat
+++ start.bat
@@ -4,6 +4,9 @@
 @REM start client
 start cmd /k "cd .\client && npm run dev"
 
+@REM start server_js
+start cmd /k "cd .\server_js && npm run dev"
+
 @REM start server_py
 
 start cmd /k "cd .\server_py && call venv\Scripts\activate.bat && uvicorn server:app --reload"
