cd0c9a21deedfd1bde08be8725b29b2ef58bcda8
sree-r-one <sreeraj_ec@hotmail.com>
2023-09-15 01:41:10 +0800

    setup server_js and setup mongoose

--- server_js/server.js
+++ server_js/server.js
@@ -1,10 +1,30 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+const connectDB = require("./db/connect");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(5000, () => {
-  console.log("Example app listening on port 5000!");
-});
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => console.log(`listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
